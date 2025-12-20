082eec66aa50b8f14cb9a8698b7d21e8414ecb39
S-Arafin <arafin23103@gmail.com>
2025-12-20 15:33:54 +0600

    add deleting frature for issues

--- index.js
+++ index.js
@@ -162,6 +162,13 @@ async function run() {
         res.send(result);
     });
 
+    app.delete('/issues/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await issuesCollection.deleteOne(query);
+        res.send(result);
+    });
+
     app.get("/my-issues/:email", async (req, res) => {
       const email = req.params.email;
       const result = await issuesCollection
